ab5c518419c45d139a4ca0b8d66d6a358257613a
David Oliván Malagón <david.olivan.93@gmail.com>
2022-11-24 21:22:15 +0000

    Adding docstrings in Task, pylint changes, ordering modules

--- lib/Task.py
+++ lib/Task.py
@@ -1,4 +1,7 @@
 class Task:
+    """
+    A class that contains the basic building blocks of a Task. Name, priority and an id.
+    """
     def __init__(self, name: str, priority: int, the_id: int):
         self.name = name
         self.priority = priority
@@ -8,25 +11,61 @@ class Task:
         return f"Task: {self.name} \tPriority: {self.priority}"
 
     @property
-    def name(self):
+    def name(self) -> str:
+        """
+        :param self: The object
+        :self type: Task
+        :return: The name property
+        :rtype: str
+        """
         return self._name
 
     @name.setter
     def name(self, name: str):
+        """
+        :param self: The object
+        :self type: Task
+        :param name: The name description of the task
+        :name type: str
+        """
         self._name = name
 
     @property
-    def priority(self):
+    def priority(self) -> int:
+        """
+        :param self: The object
+        :self type: Task
+        :return: The priority property
+        :rtype: int
+        """
         return self._priority
 
     @priority.setter
     def priority(self, priority: int):
+        """
+        :param self: The object
+        :self type: Task
+        :param name: The priority of the task
+        :name type: int
+        """
         self._priority = priority
 
     @property
-    def the_id(self):
+    def the_id(self) -> int:
+        """
+        :param self: The object
+        :self type: Task
+        :return: The id property
+        :rtype: int
+        """
         return self._the_id
 
     @the_id.setter
     def the_id(self, the_id: int):
+        """
+        :param self: The object
+        :self type: Task
+        :param name: The priority of the task
+        :name type: int
+        """
         self._the_id = the_id
--- project.py
+++ project.py
@@ -1,13 +1,21 @@
-import re, sys, csv, os
+"""Module providing function to check str patterns"""
+import re
+import sys
+import csv
+import os
 import argparse
-from lib.Task import Task
 from time import sleep
 from tabulate import tabulate
+from lib.Task import Task
 
-app_name = "The Minimalist Tasker"
+
+APP_NAME = "The Minimalist Tasker"
 
 
 def main():
+    """
+    The main function of the app
+    """
     db_name = parse_arguments()
 
     if db_name == "no-database":
@@ -54,15 +62,19 @@ def accept_command(cmd: str, tasks: list, database):
     """
     match cmd.strip().lower():
         case "exit":
-            if input("Do you want to save the current tasks before exiting? (yes/no) ").strip().lower() in ["yes", "y"]:
+            if input(
+                "Do you want to save the current tasks before exiting? (yes/no) "
+            ).strip().lower() in ["yes", "y"]:
                 save_tasks(tasks, database)
             clear_screen()
-            sys.exit(f"Thank you for using {app_name}.")
+            sys.exit(f"Thank you for using {APP_NAME}.")
         case "new":
             tasks.append(new_task(tasks))
             new_screen(tasks)
         case "save":
-            if input("Saving will overwrite the file. Are you sure you want to save? (yes/no) ").strip().lower() in ["yes", "y"]:
+            if input(
+                "Saving will overwrite the file. Are you sure you want to save? (yes/no) "
+            ).strip().lower() in ["yes", "y"]:
                 save_tasks(tasks, database)
                 new_screen(tasks, "The current tasks have been saved.")
             else:
@@ -73,6 +85,7 @@ def accept_command(cmd: str, tasks: list, database):
         case _:
             new_screen(tasks, "That is not a valid command.")
 
+
 def get_commands():
     """
     Displays all available commands that can be given to the program
@@ -82,9 +95,11 @@ def get_commands():
     """
     return "\nCommands:\t new\t save\t remove\t exit\n"
 
+
 def new_task(tasks: list) -> Task:
     """
-    Creates a new task, checks that the name is not longer than 100 char and priority is an int 1-5 and returns it
+    Creates a new task, checks that the name is not longer than 100 char
+    and priority is an int 1-5 and returns it
 
     :param tasks: The list of tasks currently available
     :type tasks: list
@@ -108,7 +123,8 @@ def new_task(tasks: list) -> Task:
                             current_ids.append(task.the_id)
 
                         the_id = 0
-                        while the_id in current_ids: the_id += 1
+                        while the_id in current_ids:
+                            the_id += 1
 
                         return Task(name, priority, the_id)
                     else:
@@ -117,6 +133,7 @@ def new_task(tasks: list) -> Task:
                     print("The priority is not a number.")
         else:
             print("Name is longer than 100 characters.")
+
 
 def remove_task(tasks: list) -> list:
     """
@@ -148,7 +165,8 @@ def remove_task(tasks: list) -> list:
 
 def display_tasks(tasks: list):
     """
-    Uses the list of Task objects to display available tasks for the user in order of priority with an index number
+    Uses the list of Task objects to display available tasks for the user
+    in order of priority with an index number
 
     :param tasks: The list of Task objects with the tasks
     :type tasks: list
@@ -160,7 +178,7 @@ def display_tasks(tasks: list):
     print(tabulate(desglosada, headers=["Id", "Priority", "Name"]))
 
 
-def sort_by_priority(ls):
+def sort_by_priority(lista):
     """
     Uses the sorted function to sort tasks by priority as a key
 
@@ -169,34 +187,38 @@ def sort_by_priority(ls):
     :return: The sorted list
     :rtype: list
     """
-    return sorted(ls, key=lambda task: task.priority)
+    return sorted(lista, key=lambda task: task.priority)
 
 
 def save_tasks(tasks: list, database: str):
     """
     Saves the files being worked on on the .csv database
     """
-    with open(f"users/{database}.csv", "w", newline="") as csvfile:
+    with open(f"users/{database}.csv", "w", newline="", encoding="utf-8") as csvfile:
         fieldnames = ["name", "priority", "id"]
         writer = csv.DictWriter(csvfile, fieldnames=fieldnames)
         writer.writeheader()
         for task in tasks:
-            writer.writerow({"name": task.name, "priority": task.priority, "id": task.the_id})
+            writer.writerow(
+                {"name": task.name, "priority": task.priority, "id": task.the_id}
+            )
 
     print("List of tasks saved.")
     sleep(1)
+
 
 def load_tasks(database: str) -> list:
     """
     Opens the csv file, loads the tasks and return them as a list of Task objects
     """
     tasks = []
-    with open(f"users/{database}.csv", newline="") as csvfile:
+    with open(f"users/{database}.csv", newline="", encoding="utf-8") as csvfile:
         reader = csv.DictReader(csvfile)
         for row in reader:
             tasks.append(Task(row["name"], int(row["priority"]), int(row["the_id"])))
 
     return tasks
+
 
 def new_screen(tasks: list, message: str = None):
     """
@@ -209,10 +231,12 @@ def new_screen(tasks: list, message: str = None):
     :type message: str
     """
     clear_screen()
-    print(get_title(app_name))
+    print(get_title(APP_NAME))
     display_tasks(tasks)
     print(get_commands())
-    if message != None: print(message)
+    if message is not None:
+        print(message)
+
 
 def get_title(text: str) -> str:
     """
@@ -229,11 +253,13 @@ def get_title(text: str) -> str:
 
     return line1 + line2 + line3
 
+
 def clear_screen():
     """
     Clears the screen based on os without showing the os.system eeeevery time
     """
     os.system("cls" if os.name == "nt" else "clear")
+
 
 def get_name() -> str:
     """
@@ -272,7 +298,8 @@ def database_exists(name: str) -> bool:
 
 def is_name_valid(name: str) -> bool:
     """
-    Checks if a given name is valid (i.e. only letters and numbers, underscores and hyphens permitted)
+    Checks if a given name is valid
+    (i.e. only letters and numbers,underscores and hyphens permitted)
 
     :param name: The name to be examined
     :type name: str
@@ -292,7 +319,7 @@ def create_database(name: str):
     :param name: The name of the new database
     :type name: str
     """
-    with open("users/" + name + ".csv", "w", newline="") as csvfile:
+    with open("users/" + name + ".csv", "w", newline="", encoding="utf-8") as csvfile:
         fieldnames = ["name", "priority"]
         writer = csv.DictWriter(csvfile, fieldnames=fieldnames)
         writer.writeheader()
@@ -325,4 +352,4 @@ if __name__ == "__main__":
         main()
     except KeyboardInterrupt:
         clear_screen()
-        print(f"Application closed. Thank you for using {app_name}.")
+        print(f"Application closed. Thank you for using {APP_NAME}.")
